ffe3fa576279f8daad325bf37210266063091536
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-10-18 13:56:58 +0200

    Pierwsze połączenie z Hibernate

--- Code/src/main/java/Test.java
+++ Code/src/main/java/Test.java
@@ -3,26 +3,55 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+
+import entity.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Soon this will connect to qsl");
+//        System.out.println("Soon this will connect to qsl");
 
-        var url = "jdbc:postgresql://localhost:5432/";
-        var user = "postgres";
-        var pass = "postgres";
+//        var url = "jdbc:postgresql://localhost:5432/";
+//        var user = "postgres";
+//        var pass = "postgres";
+//
+//        try (Connection connection = DriverManager.getConnection(url, user, pass)) {
+//
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM public.family_members");
+//
+//            while (resultSet.next()) {
+//                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("name"), resultSet.getString("surname"));
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 
-        try (Connection connection = DriverManager.getConnection(url, user, pass)) {
+        System.out.println("Hello Hibernate");
 
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM public.family_members");
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
 
-            while (resultSet.next()) {
-                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("name"), resultSet.getString("surname"));
-            }
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Product product = new Product();
+//        product.setId(20);
+        product.setName("Czekolada2");
+
+        session.save(product);
+        t.commit();
+        System.out.println("successfully saved");
+        factory.close();
+        session.close();
     }
 }
