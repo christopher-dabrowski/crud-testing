17dae33ca054d85df8ee988bbbd39f1087dbe596
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-10-20 20:02:08 +0200

    Wczytanie wszystkich tabeli

--- Code/src/main/java/CRUD.java
+++ Code/src/main/java/CRUD.java
@@ -1,5 +1,7 @@
 import entity.FamilyMember;
 import entity.Product;
+import entity.Purchase;
+import entity.ShoppingList;
 import org.hibernate.*;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
@@ -17,13 +19,10 @@ public class CRUD {
         Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
         return meta.getSessionFactoryBuilder().build();
     }
-
-    public static void main(String[] args) {
-        readTest();
-    }
-
-    private static void readTest() {
+    
+    private static List<Product> listProducts() {
         Session session = buildSessionFactory().openSession();
+        List<Product> result = null;
 
         try {
             Transaction transaction = session.beginTransaction();
@@ -31,18 +30,81 @@ public class CRUD {
             CriteriaQuery<Product> criteria = criteriaBuilder.createQuery(Product.class);
             criteria.from(Product.class);
 
-            List<Product> products = session.createQuery(criteria).getResultList();
-            session.close();
-
-            for(Product product : products) {
-                System.out.println(product.getName());
-            }
-
-//            Criteria criteria = session.createCriteria(FamilyMember.class);
+            result = session.createQuery(criteria).getResultList();
         } catch (HibernateException e) {
             e.printStackTrace();
             session.getTransaction().rollback();
         }
+        finally {
+            session.close();
+        }
 
+        return result;
+    }
+
+    private static List<FamilyMember> listFamilyMembers() {
+        Session session = buildSessionFactory().openSession();
+        List<FamilyMember> result = null;
+
+        try {
+            Transaction transaction = session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<FamilyMember> criteria = criteriaBuilder.createQuery(FamilyMember.class);
+            criteria.from(FamilyMember.class);
+
+            result = session.createQuery(criteria).getResultList();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        finally {
+            session.close();
+        }
+
+        return result;
+    }
+
+    private static List<Purchase> listPurchases() {
+        Session session = buildSessionFactory().openSession();
+        List<Purchase> result = null;
+
+        try {
+            Transaction transaction = session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Purchase> criteria = criteriaBuilder.createQuery(Purchase.class);
+            criteria.from(Purchase.class);
+
+            result = session.createQuery(criteria).getResultList();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        finally {
+            session.close();
+        }
+
+        return result;
+    }
+
+    private static List<ShoppingList> listShpppingListItems() {
+        Session session = buildSessionFactory().openSession();
+        List<ShoppingList> result = null;
+
+        try {
+            Transaction transaction = session.beginTransaction();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<ShoppingList> criteria = criteriaBuilder.createQuery(ShoppingList.class);
+            criteria.from(ShoppingList.class);
+
+            result = session.createQuery(criteria).getResultList();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        finally {
+            session.close();
+        }
+
+        return result;
     }
 }
