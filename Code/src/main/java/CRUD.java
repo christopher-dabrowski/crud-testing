ab2c4cce2e1532a7720a261796c5733b79cb4b40
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-10-20 19:07:37 +0200

    Fabryka sesji

--- Code/src/main/java/CRUD.java
+++ Code/src/main/java/CRUD.java
@@ -1,2 +1,17 @@
+import entity.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class CRUD {
+    
+    private static SessionFactory buildSessionFactory() {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+        return meta.getSessionFactoryBuilder().build();
+    }
 }
