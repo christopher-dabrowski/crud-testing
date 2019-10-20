9db7697f4f7e3e60ce130bea68dc13448e9625fa
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-10-20 18:58:01 +0200

    Encja FamilyMember

---
+++ Code/src/main/java/entity/FamilyMember.java
@@ -0,0 +1,39 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "family_member")
+public class FamilyMember {
+
+    @Id
+    private int id;
+    private String name;
+    private String surname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
