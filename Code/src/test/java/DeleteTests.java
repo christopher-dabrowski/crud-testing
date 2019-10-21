fe93bfb9f89c92c4a6537ee499731a051cd0381a
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-10-21 11:25:16 +0200

    Test D ShoppingList

--- Code/src/test/java/DeleteTests.java
+++ Code/src/test/java/DeleteTests.java
@@ -85,4 +85,64 @@ public class DeleteTests {
 
         //Then no exception
     }
+
+    @Test
+    public void deleteCreatedShoppingListItem() {
+        //Given
+        SessionFactory sessionFactory = CRUD.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        int hopefullyFreeIdProduct = 5469703;
+//        int hopefullyFreeIdShoppingItem = hopefullyFreeIdProduct + 1;
+
+        Product tmpProduct = new Product("Szczotka do zębów");
+        tmpProduct.setId(hopefullyFreeIdProduct);
+
+        ShoppingList toDelete = new ShoppingList(hopefullyFreeIdProduct, 3);
+        toDelete.setProduct_id(hopefullyFreeIdProduct);
+
+        try {
+            transaction = session.beginTransaction();
+
+            session.save(tmpProduct);
+            session.save(toDelete);
+
+            transaction.commit();
+        }
+        catch (Exception e) {
+            if (transaction!=null)
+                transaction.rollback();
+            throw e;
+        }
+        finally {
+            session.close();
+        }
+
+        //When
+        try {
+            CRUD.deleteShoppingListItem(hopefullyFreeIdProduct);
+        } catch (Exception e) { //Then no exception
+            fail();
+        } finally { //Cleanup
+            try {
+                session = sessionFactory.openSession();
+                transaction = session.beginTransaction();
+
+                session.delete(tmpProduct);
+
+                transaction.commit();
+            }
+            catch (Exception e) {
+                if (transaction!=null)
+                    transaction.rollback();
+                throw e;
+            }
+            finally {
+                session.close();
+            }
+        }
+
+    }
 }
